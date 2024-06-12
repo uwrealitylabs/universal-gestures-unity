90abe2665a190f0a570b02477da60bbe83667200
Justin Lin <tojustinlin@gmail.com>
2024-06-11 21:33:07 -0400

    Working: get finger feature

--- Assets/Scripts/TestingSkeleton.cs
+++ Assets/Scripts/TestingSkeleton.cs
@@ -1,24 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Oculus.Interaction;
+using Oculus.Interaction.PoseDetection;
+using Oculus.Interaction.Input;
 
 public class TestingSkeleton : MonoBehaviour
 {
     public GameObject rightHand;
+    public GameObject rightHandFeature;
+    private FingerFeatureStateProvider rightFingersFeatureProvider;
 
     // Start is called before the first frame update
     void Start()
     {
-        OVRSkeleton skeleton = rightHand.GetComponent<OVRSkeleton>();
-        int currentBones = skeleton.GetCurrentNumBones();
-        Debug.Log("Current number of bones: " + currentBones);
+        // OVRSkeleton skeleton = rightHand.GetComponent<OVRSkeleton>();
+        // int currentBones = skeleton.GetCurrentNumBones();
+        // Debug.Log("Current number of bones: " + currentBones);
 
-        
+        rightFingersFeatureProvider = rightHandFeature.GetComponent<FingerFeatureStateProvider>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-    }
+        float indexFingerCurl = rightFingersFeatureProvider.GetFeatureValue(HandFinger.Index, FingerFeature.Curl) ?? 0.0f;
+        Debug.Log("Index finger curl: " + indexFingerCurl);
+    }   
 }
