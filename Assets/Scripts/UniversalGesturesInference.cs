aa18a3a426cc2d4786eee7dad2443e6f122b6a3c
Anthony3301 <anthonypirvuti@gmail.com>
2024-07-22 20:00:55 -0400

    Added inference script and trained model data (need to test)

---
+++ Assets/Scripts/UniversalGesturesInference.cs
@@ -0,0 +1,87 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using Newtonsoft.Json.Linq; // idk if its good to have this installed
+
+public class UniversalGesturesInference : MonoBehaviour {
+    public GameObject handObject;
+    public string modelName;
+    private Dictionary<string, float[,]> weights;
+
+    void Start() {
+        LoadWeights("../JsonData/modelWeights.json");
+    }
+
+    void LoadWeights(string filePath) {
+        try {
+            string jsonString = File.ReadAllText(filePath);
+            JObject json = JObject.Parse(jsonString);
+
+            weights = new Dictionary<string, float[,]>();
+
+            foreach (var item in json) {
+                string key = item.Key;
+                JArray valueArray = (JArray)item.Value;
+                int rows = valueArray.Count;
+                int cols = valueArray[0].Count();
+                float[,] valueMatrix = new float[rows, cols];
+
+                for (int i = 0; i < rows; i++) {
+                    for (int j = 0; j < cols; j++) {
+                        valueMatrix[i, j] = (float)valueArray[i][j];
+                    }
+                }
+
+                weights[key] = valueMatrix;
+            }
+        }
+        catch (Exception e) {
+            Debug.LogError("Error loading weights: " + e.Message);
+        }
+    }
+
+    public float GetInference() {
+        float[] inputVector = CreateInputVector();
+        float output = RunInference(inputVector);
+        return output;
+    }
+
+    float[] CreateInputVector() {
+        List<float> inputVector = new List<float>();
+
+        // Im assuming that handObject has a component HandData (the unlabeled data from the training jsons)
+        // check if this is right
+        HandData handData = handObject.GetComponent<HandData>();
+        if (handData != null) {
+            inputVector.AddRange(handData.GetHandData());
+        } else {
+            Debug.LogError("HandData script is missing on the handObject.");
+        }
+
+        return inputVector.ToArray();
+    }
+
+    float RunInference(float[] inputVector) {
+        // again also assuming a two-layer neural network, can change to be general if needed (did this based off test data)
+        float[] fc1Output = new float[weights["fc1.weight"].GetLength(0)];
+        
+        for (int i = 0; i < fc1Output.Length; i++) {
+            for (int j = 0; j < inputVector.Length; j++) {
+                fc1Output[i] += inputVector[j] * weights["fc1.weight"][i, j];
+            }
+            fc1Output[i] = Mathf.Tanh(fc1Output[i]); // Activation function (not too sure, test to see if it works)
+        }
+
+        float[] fc2Output = new float[weights["fc2.weight"].GetLength(0)];
+        
+        for (int i = 0; i < fc2Output.Length; i++) {
+            for (int j = 0; j < fc1Output.Length; j++) {
+                fc2Output[i] += fc1Output[j] * weights["fc2.weight"][i, j];
+            }
+            fc2Output[i] = Mathf.Tanh(fc2Output[i]); // Activation function (not sure same as above)
+        }
+
+        return fc2Output[0];
+    }
+}
