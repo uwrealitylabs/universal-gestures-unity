15d167d294655f3dace6985962c53f2f1be72e4b
BrianZhang1 <brianzhangbz@gmail.com>
2024-09-29 13:00:13 -0400

    add script to collect two-hand gesture data

---
+++ Assets/Scripts/TestingSkeletonTwoHands.cs
@@ -0,0 +1,117 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Oculus.Interaction;
+using Oculus.Interaction.PoseDetection;
+using Oculus.Interaction.Input;
+
+public class TestingSkeletonTwoHands : MonoBehaviour
+{
+    public GameObject leftHand;
+    public GameObject leftHandFeature;
+    public GameObject rightHand;
+    public GameObject rightHandFeature;
+    private FingerFeatureStateProvider leftFingersFeatureProvider;
+    private FingerFeatureStateProvider rightFingersFeatureProvider;
+    public const int NUM_FEATURES = 17;
+    public static float[] handData;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // OVRSkeleton skeleton = rightHand.GetComponent<OVRSkeleton>();
+        // int currentBones = skeleton.GetCurrentNumBones();
+        // Debug.Log("Current number of bones: " + currentBones);
+
+        leftFingersFeatureProvider = leftHandFeature.GetComponent<FingerFeatureStateProvider>();
+        rightFingersFeatureProvider = rightHandFeature.GetComponent<FingerFeatureStateProvider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // ========================================
+        // LEFT HAND FEATURES
+        // ========================================
+        float leftIndexFingerCurl = leftFingersFeatureProvider.GetFeatureValue(HandFinger.Index, FingerFeature.Curl) ?? 0.0f;
+        float leftIndexFingerAbduction = leftFingersFeatureProvider.GetFeatureValue(HandFinger.Index, FingerFeature.Abduction) ?? 0.0f;
+        float leftIndexFingerFlexion = leftFingersFeatureProvider.GetFeatureValue(HandFinger.Index, FingerFeature.Flexion) ?? 0.0f;
+        float leftIndexFingerOpposition = leftFingersFeatureProvider.GetFeatureValue(HandFinger.Index, FingerFeature.Opposition) ?? 0.0f;
+
+        float leftThumbFingerCurl = leftFingersFeatureProvider.GetFeatureValue(HandFinger.Thumb, FingerFeature.Curl) ?? 0.0f;
+        float leftThumbFingerAbduction = leftFingersFeatureProvider.GetFeatureValue(HandFinger.Thumb, FingerFeature.Abduction) ?? 0.0f;
+        // Flexion, Opposition not available on thumb
+
+        float leftMiddleFingerCurl = leftFingersFeatureProvider.GetFeatureValue(HandFinger.Middle, FingerFeature.Curl) ?? 0.0f;
+        float leftMiddleFingerAbduction = leftFingersFeatureProvider.GetFeatureValue(HandFinger.Middle, FingerFeature.Abduction) ?? 0.0f;
+        float leftMiddleFingerFlexion = leftFingersFeatureProvider.GetFeatureValue(HandFinger.Middle, FingerFeature.Flexion) ?? 0.0f;
+        float leftMiddleFingerOpposition = leftFingersFeatureProvider.GetFeatureValue(HandFinger.Middle, FingerFeature.Opposition) ?? 0.0f;
+
+        float leftRingFingerCurl = leftFingersFeatureProvider.GetFeatureValue(HandFinger.Ring, FingerFeature.Curl) ?? 0.0f;
+        float leftRingFingerAbduction = leftFingersFeatureProvider.GetFeatureValue(HandFinger.Ring, FingerFeature.Abduction) ?? 0.0f;
+        float leftRingFingerFlexion = leftFingersFeatureProvider.GetFeatureValue(HandFinger.Ring, FingerFeature.Flexion) ?? 0.0f;
+        float leftRingFingerOpposition = leftFingersFeatureProvider.GetFeatureValue(HandFinger.Ring, FingerFeature.Opposition) ?? 0.0f;
+
+        float leftPinkyFingerCurl = leftFingersFeatureProvider.GetFeatureValue(HandFinger.Pinky, FingerFeature.Curl) ?? 0.0f;
+        // Pinky does not support abduction
+        float leftPinkyFingerFlexion = leftFingersFeatureProvider.GetFeatureValue(HandFinger.Pinky, FingerFeature.Flexion) ?? 0.0f;
+        float leftPinkyFingerOpposition = leftFingersFeatureProvider.GetFeatureValue(HandFinger.Pinky, FingerFeature.Opposition) ?? 0.0f;
+
+
+        // ========================================
+        // RIGHT HAND FEATURES
+        // ========================================
+        float rightIndexFingerCurl = rightFingersFeatureProvider.GetFeatureValue(HandFinger.Index, FingerFeature.Curl) ?? 0.0f;
+        float rightIndexFingerAbduction = rightFingersFeatureProvider.GetFeatureValue(HandFinger.Index, FingerFeature.Abduction) ?? 0.0f;
+        float rightIndexFingerFlexion = rightFingersFeatureProvider.GetFeatureValue(HandFinger.Index, FingerFeature.Flexion) ?? 0.0f;
+        float rightIndexFingerOpposition = rightFingersFeatureProvider.GetFeatureValue(HandFinger.Index, FingerFeature.Opposition) ?? 0.0f;
+
+        float rightThumbFingerCurl = rightFingersFeatureProvider.GetFeatureValue(HandFinger.Thumb, FingerFeature.Curl) ?? 0.0f;
+        float rightThumbFingerAbduction = rightFingersFeatureProvider.GetFeatureValue(HandFinger.Thumb, FingerFeature.Abduction) ?? 0.0f;
+        // Flexion, Opposition not available on thumb
+
+        float rightMiddleFingerCurl = rightFingersFeatureProvider.GetFeatureValue(HandFinger.Middle, FingerFeature.Curl) ?? 0.0f;
+        float rightMiddleFingerAbduction = rightFingersFeatureProvider.GetFeatureValue(HandFinger.Middle, FingerFeature.Abduction) ?? 0.0f;
+        float rightMiddleFingerFlexion = rightFingersFeatureProvider.GetFeatureValue(HandFinger.Middle, FingerFeature.Flexion) ?? 0.0f;
+        float rightMiddleFingerOpposition = rightFingersFeatureProvider.GetFeatureValue(HandFinger.Middle, FingerFeature.Opposition) ?? 0.0f;
+
+        float rightRingFingerCurl = rightFingersFeatureProvider.GetFeatureValue(HandFinger.Ring, FingerFeature.Curl) ?? 0.0f;
+        float rightRingFingerAbduction = rightFingersFeatureProvider.GetFeatureValue(HandFinger.Ring, FingerFeature.Abduction) ?? 0.0f;
+        float rightRingFingerFlexion = rightFingersFeatureProvider.GetFeatureValue(HandFinger.Ring, FingerFeature.Flexion) ?? 0.0f;
+        float rightRingFingerOpposition = rightFingersFeatureProvider.GetFeatureValue(HandFinger.Ring, FingerFeature.Opposition) ?? 0.0f;
+
+        float rightPinkyFingerCurl = rightFingersFeatureProvider.GetFeatureValue(HandFinger.Pinky, FingerFeature.Curl) ?? 0.0f;
+        // Pinky does not support abduction
+        float rightPinkyFingerFlexion = rightFingersFeatureProvider.GetFeatureValue(HandFinger.Pinky, FingerFeature.Flexion) ?? 0.0f;
+        float rightPinkyFingerOpposition = rightFingersFeatureProvider.GetFeatureValue(HandFinger.Pinky, FingerFeature.Opposition) ?? 0.0f;
+
+
+        // ========================================
+        // TWO-HAND RELATIVE FEATURES
+        // ========================================
+        float leftX = leftHand.transform.position[0];
+        float leftY = leftHand.transform.position[1];
+        float leftZ = leftHand.transform.position[2];
+        float rightX = rightHand.transform.position[0];
+        float rightY = rightHand.transform.position[1];
+        float rightZ = rightHand.transform.position[2];
+        float xDiff = rightX - leftX;
+        float yDiff = rightY - leftY;
+        float zDiff = rightZ - leftZ;
+        float leftRotation1 = leftHand.transform.rotation[0];
+        float leftRotation2 = leftHand.transform.rotation[1];
+        float leftRotation3 = leftHand.transform.rotation[2];
+        float leftRotation4 = leftHand.transform.rotation[3];
+        float rightRotation1 = rightHand.transform.rotation[0];
+        float rightRotation2 = rightHand.transform.rotation[1];
+        float rightRotation3 = rightHand.transform.rotation[2];
+        float rightRotation4 = rightHand.transform.rotation[3];
+
+        // Debug.Log("Index finger values: " + rightIndexFingerCurl + ", " + rightIndexFingerAbduction + ", " + rightIndexFingerFlexion + ", " + rightIndexFingerOpposition);
+
+        // Add values to JsonWriter.GestureData.handData
+        handData = new[] { rightThumbFingerCurl, rightThumbFingerAbduction, rightIndexFingerCurl, rightIndexFingerAbduction, rightIndexFingerFlexion, rightIndexFingerOpposition, rightMiddleFingerCurl, rightMiddleFingerAbduction, rightMiddleFingerFlexion, rightMiddleFingerOpposition, rightRingFingerCurl, rightRingFingerAbduction, rightRingFingerFlexion, rightRingFingerOpposition, rightPinkyFingerCurl, rightPinkyFingerFlexion, rightPinkyFingerOpposition,
+                   leftThumbFingerCurl, leftThumbFingerAbduction, leftIndexFingerCurl, leftIndexFingerAbduction, leftIndexFingerFlexion, leftIndexFingerOpposition, leftMiddleFingerCurl, leftMiddleFingerAbduction, leftMiddleFingerFlexion, leftMiddleFingerOpposition, leftRingFingerCurl, leftRingFingerAbduction, leftRingFingerFlexion, leftRingFingerOpposition, leftPinkyFingerCurl, leftPinkyFingerFlexion, leftPinkyFingerOpposition,
+                   xDiff, yDiff, zDiff, leftRotation1, leftRotation2, leftRotation3, leftRotation4, rightRotation1, rightRotation2, rightRotation3, rightRotation4};
+    }
+}
