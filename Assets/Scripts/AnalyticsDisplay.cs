b4f070740aaef7c63509ad58183d1cd2c2d4aaaa
sandera <shuang2322@gmail.com>
2024-10-23 20:13:17 -0400

    Add performance analytics (local to unity)

--- Assets/Scripts/AnalyticsDisplay.cs
+++ Assets/Scripts/AnalyticsDisplay.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using Unity.Profiling;
+using UnityEngine.Profiling;
 
 public class AnalyticsDisplay : MonoBehaviour
 {
@@ -15,14 +17,57 @@ public class AnalyticsDisplay : MonoBehaviour
     private UniversalGesturesInference inference;
     private URWLHandPoseDetection confidence;
 
+    private ProfilerRecorder mainThreadTimeRecorder;
+    private ProfilerRecorder cpuTotalTimeRecorder;
+
+    private int timer;
+    private static int cpuUpdateInterval = 100;
+
     private void Start()
     {
         inference = scripts.GetComponent<UniversalGesturesInference>();
         confidence = scripts.GetComponent<URWLHandPoseDetection>();
+        timer = 0;
     }
     private void Update()
     {
-        inferenceOutputText.text = "Inference Output: " + inference.inferenceOutput;
-        thresholdConfidenceText.text = "Threshold Confidence: " + confidence.getThresholdConfidence();
+        timer--;
+
+        inferenceOutputText.text = string.Format("Inference Output: {0:0.00000}", inference.inferenceOutput);
+        thresholdConfidenceText.text = string.Format("Threshold Confidence: {0:0.00000}", confidence.getThresholdConfidence());
+        latencyText.text = string.Format("{0:0.00000}ms", inference.latency);
+
+        //CPU
+        if (timer <= 0)
+        {
+            timer = cpuUpdateInterval;
+            CPUText.text = string.Format("{0:0.00}%", GetCpuPercent());
+        }
+
+        RAMUsageText.text =  GetRamUsage() + "MB";
+        
+    }
+    private float GetCpuPercent()
+    {
+        // CPU calculations
+        FrameTimingManager.CaptureFrameTimings();
+        long cpuMainThreadFrameTime = mainThreadTimeRecorder.LastValue;
+        long cpuTotalTime = cpuTotalTimeRecorder.LastValue;
+
+        return (float) cpuMainThreadFrameTime / cpuTotalTime * 100.0f;
+    } 
+    private long GetRamUsage()
+    {
+        return Profiler.GetTotalAllocatedMemoryLong() / (1024 * 1024);
+    }
+    private void OnEnable()
+    {
+        mainThreadTimeRecorder = ProfilerRecorder.StartNew(ProfilerCategory.Internal, "CPU Main Thread Frame Time");
+        cpuTotalTimeRecorder = ProfilerRecorder.StartNew(ProfilerCategory.Internal, "CPU Total Frame Time");
+    }
+    private void OnDisable()
+    {
+        mainThreadTimeRecorder.Dispose();
+        cpuTotalTimeRecorder.Dispose();
     }
 }
