d3ad8f996cd4980ce25a05f138f11398cd542f28
Alex J <94213401+alexjiang903@users.noreply.github.com>
2025-08-16 15:20:04 -0600

    uploaded new onnx with fc3_1 directly exposed, added L2 Normalization to fingerprint

--- Assets/Scripts/PrototypeGestureRecog.cs
+++ Assets/Scripts/PrototypeGestureRecog.cs
@@ -9,29 +9,47 @@ public class PrototypeGestureRecog : MonoBehaviour
     [SerializeField] private Model runtime_model;
     [SerializeField] private IWorker worker;
 
-
     void Start()
     {
         runtime_model = ModelLoader.Load(trained_mlp);
-        worker = WorkerFactory.CreateWorker(WorkerType.Compute, runtime_model);
+        worker = WorkerFactory.CreateWorker(WorkerFactory.Type.Auto, runtime_model);
     }
 
 
-    public float[] run_inference(float[] input_arr)
+    public float[] RunInference(float[] input_arr, bool l2_normalize = true)
     {
         Tensor input_tensor = new Tensor(1, input_arr.Length, input_arr);
         worker.Execute(input_tensor);
-        Tensor output_tensor = worker.PeekOutput(target_layer_name); // final output (not used
-        Tensor n_dim_fingerprint = worker.PeekOutput("fc3_1"); // layer containing N-dim fingerprint 
-        float[] activation_data = n_dim_fingerprint.ToReadOnlyArray(); //final output data transferred to array
+        Tensor n_dim_fingerprint = worker.PeekOutput(target_layer_name); // layer containing N-dim fingerprint 
+        float[] activation_data = n_dim_fingerprint.ToReadOnlyArray(); // convert tensor values into a float array for Unity to use
 
         //Clean up unused tensors to avoid memory leaks
         input_tensor.Dispose();
-        output_tensor.Dispose();
         n_dim_fingerprint.Dispose();
 
-        return activation_data;
+        if (l2_normalize)
+        {
+            activation_data = DetermineL2Norm(activation_data);
+        }
 
+        return activation_data;
+    }
+
+    private float[] DetermineL2Norm(float[] emb)
+    {
+        float norm_val = 0f;
+        for (int i = 0; i < emb.Length; i++)
+        {
+            norm_val += emb[i] * emb[i];
+        }
+
+        norm_val = Mathf.Sqrt(norm_val) + 1e-10f;
+        for (int i = 0; i < emb.Length; i++)
+        {
+            emb[i] /= norm_val;
+        }
+    
+        return emb;
     }
 
     void OnDestroy()
