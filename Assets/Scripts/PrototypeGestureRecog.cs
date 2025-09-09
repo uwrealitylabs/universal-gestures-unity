336dd132ff9693b38a34869e746543af709abcce
Alex J <94213401+alexjiang903@users.noreply.github.com>
2025-09-08 23:27:59 -0400

    added skeleton for extracting labels from the input JSON in Start method

--- Assets/Scripts/PrototypeGestureRecog.cs
+++ Assets/Scripts/PrototypeGestureRecog.cs
@@ -1,16 +1,23 @@
 using Unity.Barracuda;
 using UnityEngine;
 using System;
+using System.IO;
+using System.Text.RegularExpressions;
 using System.Collections.Generic;
+
 
 public class PrototypeGestureRecog : MonoBehaviour
 {
     [Header("Model/Layer Settings")]
     public NNModel trained_mlp;
-    public string target_layer_name = "fc3_1"; //target layer name for N-dim fingerprint 
+    public string target_layer_name = "fc3_1"; //target layer name for N-dim fingerprint
+    public string json_data_directory = "JsonData/"; // where all input JSON tensors are stored (1x17)
+    public string specific_test_json = "JsonData/thumbsupLily.json"; //testing purposes (single JSON file)
+    public bool testing_mode = true; // if true, only runs inference on specific_test_json file
 
     [SerializeField] private Model runtime_model;
     [SerializeField] private IWorker worker;
+
 
     [Serializable]
     public class GestureStats
@@ -45,7 +52,6 @@ public class PrototypeGestureRecog : MonoBehaviour
     }
 
     // Dictionary to map each gesture label to its corresponding GestureStats
-    // I am assuming that we are able to get the gesture label as a string somehow... (not sure how this will work specifically)
     private readonly Dictionary<string, GestureStats> gestureStatsDict =
     new Dictionary<string, GestureStats>(StringComparer.OrdinalIgnoreCase);
 
@@ -55,12 +61,43 @@ public class PrototypeGestureRecog : MonoBehaviour
     {
         runtime_model = ModelLoader.Load(trained_mlp);
         worker = WorkerFactory.CreateWorker(WorkerFactory.Type.Auto, runtime_model);
+
+        if (testing_mode)
+        {
+            string baseName = ExtractGestureNameFromFileName(specific_test_json);
+            Debug.Log($"Testing mode enabled. Running inference on single JSON file: {baseName}");
+            // TODOs (WIP):
+            // 1. Read JSON file to extract the handData tensor (WIP)
+            // 2. Call RunInference with the handData tensor to get the fingerprint
+            // 3. Call UpdateClass with the gestureName and fingerprint to update the class statistics
+        }
+        else
+        {
+            foreach (string path in Directory.GetFiles(json_data_directory, "*.json"))
+            {
+                string gestureName = ExtractGestureNameFromFileName(path);
+                // TODOs (WIP):
+                // 1. Read JSON file to extract the handData tensor (WIP)
+                // 2. Call RunInference with the handData tensor to get the fingerprint
+                // 3. Call UpdateClass with the gestureName and fingerprint to update the class statistics
+            }
+        }        
+    }
+
+
+    private string ExtractGestureNameFromFileName(string filePath)
+    {
+        string baseName = Path.GetFileNameWithoutExtension(filePath);
+        // split on first underscore/dash/space to isolate label prefix
+        var m = Regex.Match(baseName, @"^([^_\-\s]+)");
+        return m.Success ? m.Groups[1].Value : baseName;
     }
 
 
     // ------ Public Runtime Methods ------
     public float[] RunInference(float[] input_arr)
     {
+        // gets the N-dim fingerprint from the target layer of the MLP (128-dim)
         Tensor input_tensor = new Tensor(1, input_arr.Length, input_arr);
         worker.Execute(input_tensor);
         Tensor n_dim_fingerprint = worker.PeekOutput(target_layer_name); // layer containing N-dim fingerprint 
@@ -75,6 +112,7 @@ public class PrototypeGestureRecog : MonoBehaviour
 
     public void UpdateClass(string gestureName, float[] fingerprint)
     {
+        // get gestureName from the input JSON filename (e.g bunny_ears.json -> gestureName = "bunny_ears")
         if (!gestureStatsDict.TryGetValue(gestureName, out GestureStats stats))
         {
             stats = new GestureStats(fingerprint.Length);
